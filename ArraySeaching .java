fec1d8ec07e34f06fd809acddb13bdb5e3c23983
Chirag-Solanki6804 <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-06-30 23:38:04 +0530

    Add files via upload

---
+++ ArraySeaching .java
@@ -0,0 +1,11 @@
+public class ArraySeaching{
+	public static void main(String[] args) {
+		int a[]={10,15,20,25,27,29,34,38,47,51,60};
+		System.out.println("Which Number You Want to Search");
+		Scanner sc=new Scanner;
+		int usernumber=sc.NextInt();
+
+		
+
+	}
+}
